c6886ac36ef803391db2278049e4863037c06dba
sam reddy <samreddy659@gmail.com>
2022-10-10 12:45:46 +0530

    const temp to var

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.post("/", function(req, res){
   https.get(url, function(response){
     response.on("data", function(data){
       const weatherData = JSON.parse(data);
-      const temper = weatherData.main.temp;
+      var temper = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
 
